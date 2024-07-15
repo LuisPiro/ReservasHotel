806f09802559481e4a9fdcc9fd9737284bad194b
LuisPiro <lpiroc@gmail.com>
2024-07-15 19:25:01 -0400

    swagger2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 // Configurar Swagger
-app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Configurar las rutas
 app.use(urlBase, routes);
