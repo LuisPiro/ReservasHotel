c2d174c7df1950a9eb0f4999225db300309bdc40
LuisPiro <lpiroc@gmail.com>
2024-07-16 03:14:34 -0400

    render15

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 // Cambia la URL base para apuntar a la URL de Render o localhost si estás en desarrollo
-const apiBaseUrl = 'https://reservashotel-nul0.onrender.com';
+const apiBaseUrl = 'https://reservashotel-nul0.onrender.com/api';
 
 // Hacer la solicitud GET para obtener todas las reservas
 axios.get(`${apiBaseUrl}/api/reservations`)
