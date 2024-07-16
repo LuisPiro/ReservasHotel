c14356a8fc7a7bdd3a8cdee384e9b0479fa83245
LuisPiro <lpiroc@gmail.com>
2024-07-15 20:30:14 -0400

    Render2

--- client/client.js
+++ client/client.js
@@ -3,23 +3,11 @@ const axios = require('axios');
 // Cambia la URL base para apuntar a la URL de Render o localhost si estás en desarrollo
 const apiBaseUrl = 'https://hotel-reservation-api-j4y0.onrender.com';
 
-// Datos de la nueva reserva
-const reservationData = {
-  arrivalDate: "2024-06-16",
-  departureDate: "2024-06-20",
-  nameHotel: "Hotel Templarios",
-  typeRoom: "Double",
-  passengers: 2,
-  name: "John Doe",
-  mail: "john.doe@example.com",
-  paymentStatus: "Paid"
-};
-
-// Hacer la solicitud POST para crear una nueva reserva
-axios.post(`${apiBaseUrl}/reservations`, reservationData)
+// Hacer la solicitud GET para obtener todas las reservas
+axios.get(`${apiBaseUrl}/reservations`)
   .then(response => {
-    console.log('Reservation created successfully:', response.data);
+    console.log('All Reservations:', response.data);
   })
   .catch(error => {
-    console.error('Error creating reservation:', error);
+    console.error('Error fetching reservations:', error);
   });
